aed5401ca33e353b0b35ce5031ec9acd94dd330b
Tayssirsboui <tayssirsboui@gmail.com>
2023-03-14 15:53:22 +0100

    correct interface EntityMapper

--- src/main/java/com/nesryne/wallet/service/mapper/EntityMapper.java
+++ src/main/java/com/nesryne/wallet/service/mapper/EntityMapper.java
@@ -2,15 +2,6 @@ package com.nesryne.wallet.service.mapper;
 
 import java.util.List;
 
-<<<<<<< HEAD
-public interface EntityMapper<D,E> {
-    E toEntity(D dto);
-    D toDto(E entity);
-    List <E> toEntity(List<D> dtoList);
-    List <D> toDto(List<E> entityList);
-    
-}
-=======
 public interface EntityMapper <D, E>{
 
 
@@ -23,8 +14,3 @@ public interface EntityMapper <D, E>{
     
         List <D> toDto(List<E> entityList);
     }
-    
-    
-
-
->>>>>>> 97872aa80bb8e52c8c08285c30479868b87c1364
